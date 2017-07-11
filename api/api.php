9de1327111e9af6fa9bcf9f2f9c13c282d668894
ancabalc <anca.balc@gmail.com>
2017-07-11 17:48:28 +0300

    Create api.php

---
+++ api/api.php
@@ -0,0 +1,24 @@
+<?php
+require "configs/routes.php";
+define ("APP_FOLDER", "/api");
+$currentRoute = str_replace(APP_FOLDER, "", $_SERVER["REDIRECT_URL"]);
+if (!empty($currentRoute)) {
+    if (array_key_exists($currentRoute, $routes)) {
+        $class = $routes[$currentRoute]["class"];
+        $method = $routes[$currentRoute]["method"];
+        
+        require "api/controllers/".$class.".php";
+        $controller = new $class();
+        $response = $controller->$method();
+        
+        header("Content-Type: application/json");
+        echo json_encode($response);
+        
+    } else {
+        http_response_code(404);
+        echo "PAGE NOT FOUND!";
+    }
+} else {
+    http_response_code(403);
+    echo "ACCESS FORBIDDEN!";
+}
