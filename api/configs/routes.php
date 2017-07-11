ad77053a91578a4b88b36d3b5463ee2c37d94aa1
Nicu Lazar <lazarnicku@gmail.com>
2017-07-11 17:42:07 +0000

    Implement list users

--- api/configs/routes.php
+++ api/configs/routes.php
@@ -2,4 +2,6 @@
 
 $routes["/accounts/create"] = ["class"=>"Accounts","method"=>"signUp"];
 $routes["/applications/create"] = ["class" => "Applications", "method" => "create"];
+$routes["/users"] = ["class" => "Users", "method" => "getAll"];
+$routes["/users/last3"] = ["class" => "Users", "method" => "getLast3"];
 ?>
--- api/models/UsersModel.php
+++ api/models/UsersModel.php
@@ -1,7 +1,18 @@
 <?php
 require_once "DB.php";
 class UsersModel extends DB {
-
+    function getAll() {
+        $query = 'select name, email,role,job, description from users;'; 
+        return $this->executeQuery($query);
+    }
+    function getLast3() {
+        $query = "select id,name,email,role,job,description from users order by id desc limit 3";
+        return $this->executeQuery($query);
+    }
+    function selectUser($id) {
+       $query = 'select * from articles where id = ' . $id . ';';
+       return $this->executeQuery($query, true);    
+    }
 }
 
 ?>
