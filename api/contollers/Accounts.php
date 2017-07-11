3d353a1f55d9c81cbe029d0701a9d2a168546957
yorares <name_no_clue@yahoo.com>
2017-07-11 19:04:10 +0300

    sign up controller

--- api/contollers/Accounts.php
+++ api/contollers/Accounts.php
@@ -7,7 +7,36 @@ class Accounts {
 	{
 		$this->usersModel = new UsersModel();
 	}
+	function signUp(){
+		$err=[];
 
+		if (empty($_POST["userName"])) {
+			array_push($err,"Empty username field ");
+		}
+
+		if ($_POST["password"] != $_POST["repassword"]) {
+			array_push($err,"Password fields do not match ");
+		}elseif (empty($_POST["password"])) {
+			array_push($err,"Empty password field ");
+		}
+
+		if (empty($_POST["firstName"])) {
+			array_push($err,"Empty First Name field ");
+		}
+		if (empty($_POST["lastName"])) {
+			array_push($err,"Empty Last Name field ");
+		}
+		if (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+     		array_push($err,"Invalid email ");
+		}
+		if (empty($err)) {
+			$salt = '$1$12!abawdawdasergs';
+			$_POST["password"] = crypt($_POST["password"], $salt);
+			$id = $this->usersModel->insertItem($_POST);
+			return "Sign Up succesfull, ";
+			return "Inserted Article Id = " .$id;
+		}else{return $err;die();}
+	}
 
     }
     
