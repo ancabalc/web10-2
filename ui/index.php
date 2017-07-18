188bcf58cfa9c7c27d0cf8ebd61a6644af55811c
Florea Uwe Alexandru <floreauwe@gmail.com>
2017-07-18 17:29:59 +0000

    Add header+footer+style

--- ui/index.php
+++ ui/index.php
@@ -4,8 +4,12 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
 </head>
 <body>
+    <?php include("layout/navbar.html"); ?>
+    <?php include("layout/footer.html") ?>
+    
     <div class='offers-list'>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
