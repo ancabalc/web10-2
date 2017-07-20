64432c2104c97cf2c2e1eeab3e4f9a89d971a5dd
Florea Uwe Alexandru <floreauwe@gmail.com>
2017-07-20 16:44:49 +0000

    add slider to index

--- ui/index.php
+++ ui/index.php
@@ -7,50 +7,16 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
-<body>
-
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-      <div class="item active">
-        <img src="/ui/images/office.jpg" alt="office" style="width:100%;height:80%;">
-      </div>
-      
-      <div class="item">
-        <img src="/ui/images/desk.jpg" alt="desk" style="width:100%;height:80%;">
-      </div>
-      
-    <meta charset="UTF-8">
-    <title>Document</title>
-    <link rel="stylesheet" href="assets/css/bootstrap.min.css" type="text/css" />
-    <link rel="stylesheet" href="assets/css/style.css" type="text/css" />
+  
 </head>
 <body>
     <?php include("layout/navbar.html"); ?>
     <?php include("layout/footer.html") ?>
+    <?php include("slider.html") ?>
     
     <div class='offers-list'></div>
   
-
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
-</div>
-
+   
+<link rel="stylesheet" href="assets/css/style.css" type="text/css" />
 </body>
 </html>
